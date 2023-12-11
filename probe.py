c558ec65f8d5e6af28db6002748fcfad91f2398f
Kyle McGrath <x11kjm@gmail.com>
2023-12-11 04:36:44 -0800

    Add wandb, detailed logging

--- probe.py
+++ probe.py
@@ -1,12 +1,16 @@
 import os
 import sys
-# sys.path.append("../")  # Dirty hack
+import argparse
+
+
 
 from tqdm import tqdm
+import wandb
 import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader, RandomSampler
+import pathlib
 
 from datasets import load_dataset
 from transformers.data.data_collator import DataCollatorWithPadding
@@ -15,9 +19,6 @@ from transformers.data.data_collator import DataCollatorWithPadding
 import collate
 from vocabulary import CharVocabulary
 from evaluate_transformers import LMEvaluator
-
-
-
 
 
 
@@ -51,34 +52,59 @@ def get_final_hidden_state(evaluator, input_data, input_lengths, labels=None, de
         max_len = torch.tensor(embed.shape[1]).to(device)
 
         mask = evaluator.model.generate_len_mask(max_len, input_lengths)
-
-        # # Test code
-        # input = input_data
-        # prefix_sos = (torch.ones((input.shape[0],1)).long() * evaluator.model.encoder_sos).to(device)
-        # input = torch.cat((prefix_sos, input), dim=1) # add SOS token
-        # input_lengths += 1
-        # outputs = evaluator.model(input, input_lengths)
-        # _, preds = torch.max(outputs['data'], dim=2)
-
-        # labels_cmp = torch.cat((labels, prefix_sos), dim=1)
-        # labels_match = (preds == labels_cmp) * (input != 0) * (labels_cmp != 0)
-
-
-
-
         hidden_states = evaluator.model.encoder_only(input_data, mask, layer_id=-1)
 
-        
-        # hidden_states = evaluator.model.trafo.get_hidden_states(embed, src_length_mask=mask, is_lm=True)
-
+    
     return hidden_states.detach()
     
 
 def get_character_index(s, ch):
     return [i for i, ltr in enumerate(s) if ltr == ch]
 
+def bracket_depth(s):
+    """
+    Function to calculate the specific depth for each ')' bracket in a string, as per the new rule.
+    The depth is the maximum depth of the smallest substring that contains the ')' and is balanced.
 
+    Args:
+    s (str): The input string containing brackets.
 
+    Returns:
+    list of int: The list of depths for each ')' bracket.
+    """
+    # Helper function to find the max depth of a substring
+    def max_depth(subs):
+        current_depth = max_depth = 0
+        for char in subs:
+            if char == '(':
+                current_depth += 1
+                max_depth = max(max_depth, current_depth)
+            elif char == ')':
+                current_depth -= 1
+        return max_depth
+
+    depths = []
+    # Find all closing brackets and their respective depths
+    for i in range(len(s)):
+        if s[i] == ')':
+            # Find the matching opening bracket
+            balance = 1
+            for j in range(i - 1, -1, -1):
+                if s[j] == ')':
+                    balance += 1
+                elif s[j] == '(':
+                    balance -= 1
+                if balance == 0:
+                    # Calculate the depth of the smallest balanced substring containing this bracket
+                    depths.append(max_depth(s[j:i+1]))
+                    break
+
+    return depths
+
+def print_percent_accurate(evaluations_dict):
+    for key, value in evaluations_dict.items():
+        if value['count'] > 0:
+            print(f"{key}: {value['correct']/value['count']*100} -- correct: {value['correct']} -- count {value['count']}")
 
 def get_batch_char_idxs(strings):
     char_idxs = []
@@ -107,25 +133,37 @@ def generate_output(idxs, max_len, predictions):
 
     return output
 
-def visualize_line(string, predictions, label, char_idxs):
-
-    tensor_length = len(string)
-    view_string = generate_output(char_idxs, tensor_length, predictions)
-
+def visualize_line(strings, predictions, labels, char_idxs_batch, depth_batch, sample_size=10):
+    assert sample_size < len(strings), "Sample size larger than batch!"
     print("\nVisualizations")
     print(f"{'='*50}\n")
+    for idx in range(sample_size):
 
-    string_label = in_vocab.ind_to_str(label.tolist())
-    print(f"{string = }")
-    print(f"{view_string = }")
-    print(f"{string_label = }")
+        string = strings[idx]
+        prediction = predictions[idx]
+        label = labels[idx]
+        char_idx_row = char_idxs_batch[idx]
+        depths = depth_batch[idx]
+    
+
+        tensor_length = len(strings[idx])
+        preds_string = generate_output(char_idx_row, tensor_length, prediction)
+        depth_string = generate_output(char_idx_row, tensor_length, depths)
 
 
-def train(evaluator, classifier, dataset, optimizer, criterion, device='cpu', eval_steps=1000):
+
+        string_label = in_vocab.ind_to_str(label.tolist())
+        print(f"{string =       }")
+        print(f"{preds_string = }")
+        print(f"{string_label = }")
+        print(f"{depth_string = }\n")
+
+
+def train(evaluator, classifier, dataset, optimizer, criterion, device='cpu', step=None, eval_steps=1000):
     classifier.train()
     evaluator.model.eval()  # Set evaluator to evaluation mode
 
-    step = 0
+
     train_dataloader = get_dataloader(dataset['train'], collate_fn=collator, batch_size=64)
     counts = {k: 0 for k in range(10)}
     for batch in tqdm(train_dataloader, desc="Training"):
@@ -140,68 +178,82 @@ def train(evaluator, classifier, dataset, optimizer, criterion, device='cpu', ev
 
         for row_idx, char_batch in enumerate(char_idxs):
             if char_batch:
-                for char_idx in char_batch:
+                label = labels[row_idx][char_batch] - 5 # Hack shift 
+                outputs = classifier(hidden_states[row_idx, char_batch])
+                loss = criterion(outputs, label)
 
-                    label = labels[row_idx][char_idx] - 5 # Hack shift 
-                    counts[int(label)] += 1
-
-
-                        
-
-
-                    outputs = classifier(hidden_states[row_idx, char_idx])
-
-                    
-                    loss = criterion(outputs, label)
-
-                    loss.backward()  # Perform a single backward pass
-                    optimizer.step()  # Update parameters
-                    optimizer.zero_grad()
+                loss.backward()  # Perform a single backward pass
+                optimizer.step()  # Update parameters
+                optimizer.zero_grad()
 
 
         if step % eval_steps == 0:
             test_dataloader = get_dataloader(dataset['test'], collate_fn=collator, batch_size=64)
-            avg_loss, accuracy = evaluate(evaluator, classifier, test_dataloader, criterion, device=device)
+            avg_loss, accuracy, evaluations = evaluate(evaluator, classifier, test_dataloader, criterion, device=device, visualize=True)
             print(f"{step = }: {avg_loss = }, {accuracy = }")
+            print_percent_accurate(evaluations)
+            wandb.log({"loss": avg_loss, "accuracy": accuracy, "step" :step})
+            
+
+    return step
 
 
-def evaluate(evaluator, classifier, dataloader, criterion, device='cpu', visualize=False):
+def evaluate(evaluator, classifier, dataloader, criterion, device='cpu', visualize=False, viz_every=150):
     classifier.eval()  # Set the classifier to evaluation mode
     total_loss = 0.0
     correct_predictions = 0
     total_predictions = 0
+    total_iters = 0
 
+    cfg_depth = 8
+
+    evaluations = {key: {'count': 0, 'correct': 0} for key in range(cfg_depth)}
     with torch.no_grad():  # Ensure no gradients are calculated
-        for batch in tqdm(dataloader, desc="Evaluating"):
+        for batch_idx, batch in enumerate(tqdm(dataloader, desc="Evaluating")):
             inputs = batch['in'].transpose(0,1).to(device)
             input_lengths = batch['in_len'].long().to(device)
             labels = batch['labels'].transpose(0,1).long().to(device)
             strings = batch['string']
             char_idxs = get_batch_char_idxs(strings)
 
+
             hidden_states = get_final_hidden_state(evaluator, inputs, input_lengths)
-
             for row_idx, char_batch in enumerate(char_idxs):
+                
+                depths = bracket_depth(strings[row_idx])
+
                 if char_batch:
-                    predictions = []
-                    for char_idx in char_batch:
-                        outputs = classifier(hidden_states[row_idx, char_idx])
 
-                        label = labels[row_idx][char_idx] - 5
-                        loss = criterion(outputs, label)
-                        total_loss += loss.item()
+                    outputs = classifier(hidden_states[row_idx, char_batch])
 
-                        _, predicted = torch.max(outputs.data, 0)
-                        correct_predictions += (predicted == label).item()
-                        total_predictions += 1
-                        predictions.append(int(predicted))
-                    if visualize:
-                        visualize_line(strings[row_idx], predictions, labels[row_idx], batch)
+                    label = labels[row_idx][char_batch] - 5
+                    loss = criterion(outputs, label)
+                    total_loss += loss.item()
 
-    avg_loss = total_loss / total_predictions if total_predictions > 0 else 0
+                    _, predicted = torch.max(outputs.data, 1)
+                    
+                    # correct_predictions += int(sum((predicted == label)))
+                    total_predictions += len(label)
+                    total_iters +=1
+
+                    for pred_idx, prediction in enumerate(predicted):
+                        correct_int = (prediction == label[pred_idx]).item()
+                        correct_predictions += correct_int
+                        depth = depths[pred_idx]
+                        evaluations[depth]['count'] += 1
+                        evaluations[depth]['correct'] += correct_int
+
+                # if visualize and batch_idx % viz_every == 0:
+                #     visualize_line(strings, predicted, labels, char_batch, depth_batch)
+
+    avg_loss = total_loss / total_iters if total_iters > 0 else 0
     accuracy = correct_predictions / total_predictions if total_predictions > 0 else 0
 
-    return avg_loss, accuracy
+
+    accuracies_at_depth = {f"accuracy_depth_{key}": (value['correct'] / value['count']) if value['count'] > 0 else 0 for key, value in evaluations.items()}
+    wandb.log(accuracies_at_depth)
+
+    return avg_loss, accuracy, evaluations
 
 
 
@@ -216,24 +268,26 @@ def get_dataloader(dataset, batch_size = 16, collate_fn = DataCollatorWithPaddin
 
 if __name__ == '__main__':
 
-    # model_dir = '/home/x11kjm/structural-grokking/saved_models/231130_high_depth/run_nhead_4_enclayer_4_vec_512_dsheight_6_dsmin_1_regularize_False_gold_False'
-    # checkpoint = 'state_final_model.pt'
-    # dataset_dir = 'primary_dataset'
+    learning_rate = 0.0001
+    
 
-    model_dir = '/home/x11kjm/sweeps/probe_reg/run_nhead_8_enclayer_7_vec_512_dsheight_8_dsmin_1_regularize_True_gold_True'
-    checkpoint = 'state_200000.pt'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=pathlib.Path)
+    parser.add_argument("--wandb_name", type=str)
+    args = parser.parse_args()
+
+
+    saved_model = args.model
+
 
     device ='cuda'
 
-    saved_model = os.path.join(model_dir, checkpoint)
 
     in_vocab = CharVocabulary(chars=set('0123456789+*()[]xyzL=_'), ignore_char='_', ignore_char_idx=0)
     out_vocab = CharVocabulary(chars=set("0123456789_"))
     evaluator = LMEvaluator(in_vocab, out_vocab, 512, 8, 7, model_load_path=saved_model, device=device)
 
     collator = collate.VarLengthCollate(in_vocab)
-
-
 
     # dataset = load_dataset("cs330/minH_1_maxH_4_maxW_100_holdUniq_1000_regex_None_pSub_0.15_maxHeld_10000_balance_True_inter_True")
     data_files = {"train": "train/data-00000-of-00001.arrow", "test": "test/data-00000-of-00001.arrow"}
@@ -244,8 +298,14 @@ if __name__ == '__main__':
     weights = torch.tensor([0.1, 1, 1,1,1,1,1,1,1,1]).to(device)
 
     criterion = torch.nn.CrossEntropyLoss(weight=weights) 
-    optimizer = torch.optim.Adam(classifier.parameters(), lr = 0.0001)
+    optimizer = torch.optim.Adam(classifier.parameters(), lr = learning_rate)
 
+    wandb.init(project="probe", name=args.wandb_name)
+    wandb.config.update({"learning_rate": learning_rate})
+
+
+    step = 0
     for i in range(100):
-        train(evaluator, classifier, dataset, optimizer, criterion, eval_steps=1000, device=device)
+        step = train(evaluator, classifier, dataset, optimizer, criterion, eval_steps=10, step=step, device=device)
 
+    wandb.finish()
