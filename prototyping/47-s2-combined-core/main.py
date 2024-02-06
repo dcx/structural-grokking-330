c2d4de9cd510947a46ebf278802da7f93eae5370
Derek Chong <derekch@jagupard32.stanford.edu>
2024-02-06 07:25:16 -0800

    Primitive S2 via internal loop, resnet

--- prototyping/47-s2-combined-core/main.py
+++ prototyping/47-s2-combined-core/main.py
@@ -17,19 +17,24 @@ wandb_logger = WandbLogger(log_model=False)
 # GPU setup
 torch.set_float32_matmul_precision('medium')
 
+# required to run bptt over in->loop->out transfomers - not clear why
+# https://stackoverflow.com/questions/77343471/pytorch-cuda-error-invalid-configuration-argument
+torch.backends.cuda.enable_mem_efficient_sdp(False)
+torch.backends.cuda.enable_flash_sdp(False)
+torch.backends.cuda.enable_math_sdp(True)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--bs", type=int, default=32, help="Batch size")
-parser.add_argument("--lr_s1", type=float, default=1e-4, help="Learning rate for S1")
+parser.add_argument("--bs", type=int, default=8, help="Batch size")
+parser.add_argument("--lr_s1", type=float, default=1e-6, help="Learning rate for S1")
 parser.add_argument("--lr_s2", type=float, default=1e-5, help="Learning rate for S2")
 parser.add_argument("--n_train", type=int, default=1000000, help="Number of training samples")
 parser.add_argument("--n_val", type=int, default=256, help="Number of validation samples")
 parser.add_argument("--val_check_interval", type=int, default=1500, help="Validation check interval")
-parser.add_argument("--vae_checkpoint", type=str, default=None, help="Path to VAE checkpoint")
+parser.add_argument("--vae_checkpoint", type=str, default='../checkpoints/model-epoch=01-step=00567500.ckpt', help="Path to VAE checkpoint")
 parser.add_argument("--s2_checkpoint", type=str, default=None, help="Path to checkpoint for resuming full train")
 parser.add_argument("--cpu_procs", type=int, default=6, help="Number of CPU processes")
-parser.add_argument("--cuda_id", type=int, default=0, help="GPU to use")
-parser.add_argument("--n_bptt", type=int, default=2, help="Number of S2 inner loops")
+# parser.add_argument("--cuda_id", type=int, default=0, help="GPU to use")
+parser.add_argument("--n_bptt", type=int, default=4, help="Number of S2 inner loops")
 parser.add_argument("--bptt_every_loop", action="store_true", help="Backprop into each S2 loop, instead of just the last one?")
 hparams = vars(parser.parse_args())
  
@@ -53,9 +58,9 @@ hparams = vars(parser.parse_args())
 
 
 hparams['model_hparams'] = {
-    'd_model': 128,
-    'n_enc_heads': 8,
-    'n_enc_layers': 6, 
+    'd_model': 768,
+    'n_enc_heads': 12,
+    'n_enc_layers': 12, 
     'n_tokens': len(dataset.stoi), # 32 for chess
     'lr_s1': hparams['lr_s1'],
     'lr_s2': hparams['lr_s2'],
@@ -69,31 +74,31 @@ hparams['model_hparams'] = {
     'bptt_every_loop': hparams['bptt_every_loop'],
 }
 
-
-# log all hyperparameters
-wandb_logger.log_hyperparams(hparams)
-
-
 # setup data
 ds_train, ds_val = dataset.make_datasets(hparams['n_train'], hparams['n_val'], num_proc=hparams['cpu_procs'])
 collate_fn = dataset.collate_fn
-sampler_train = dataset.SeqLenAwareBatchSampler(ds_train, hparams['bs'], shuffle=True)
-sampler_val = dataset.SeqLenAwareBatchSampler(ds_val, hparams['bs'], shuffle=True)
-dl_train = data.DataLoader(ds_train, collate_fn=collate_fn, pin_memory=True, batch_sampler=sampler_train, num_workers=hparams['cpu_procs'])
-dl_val = data.DataLoader(ds_val, collate_fn=collate_fn, pin_memory=True, batch_sampler=sampler_val, num_workers=hparams['cpu_procs'])
+#sampler_train = dataset.SeqLenAwareBatchSampler(ds_train, hparams['bs'], shuffle=True)
+#sampler_val = dataset.SeqLenAwareBatchSampler(ds_val, hparams['bs'], shuffle=True)
+dl_train = data.DataLoader(ds_train, collate_fn=collate_fn, pin_memory=True, num_workers=hparams['cpu_procs'], batch_size=hparams['bs'], shuffle=True) # batch_sampler=sampler_train, 
+dl_val = data.DataLoader(ds_val, collate_fn=collate_fn, pin_memory=True, num_workers=hparams['cpu_procs'], batch_size=hparams['bs'], shuffle=False) # batch_sampler=sampler_val, 
 
 # model
 
 if hparams['vae_checkpoint'] is None:
     vae_model = model.S1Transformer(**hparams['model_hparams'])
 else:
-    vae_model = model.S1Transformer.load_from_checkpoint(hparams['vae_checkpoint']).to(f"cuda:{hparams['cuda_id']}")
+    vae_model = model.S1Transformer.load_from_checkpoint(hparams['vae_checkpoint']) # .to(f"cuda:{hparams['cuda_id']}")
 
 main_model = model_s2.S2Transformer(vae_model, **hparams['model_hparams'])
 if hparams['s2_checkpoint']:
     ckpt_path = hparams['s2_checkpoint']
 else:
     ckpt_path = None
+
+
+# log all hyperparameters, gradients and model topology
+wandb_logger.log_hyperparams(hparams)
+wandb_logger.watch(main_model, log_freq=200)
 
 
 # checkpointing
@@ -111,7 +116,7 @@ print(f"Checkpoint identifier: {fname_prefix}")
 
 # training
 trainer = L.Trainer(accelerator='gpu', logger=wandb_logger, val_check_interval=hparams['val_check_interval'], 
-                    callbacks=[checkpoint_callback], devices=[hparams['cuda_id']])
+                    callbacks=[checkpoint_callback], devices=2) # devices=[hparams['cuda_id']])
 trainer.fit(main_model, dl_train, dl_val, ckpt_path=ckpt_path)
 
 
--- prototyping/47-s2-combined-core/model_s2.py
+++ prototyping/47-s2-combined-core/model_s2.py
@@ -116,7 +116,7 @@ class S2Model(nn.Module):
         _, bs_sl, d_model = x_enc.shape
 
         # prep output tensor
-        s2_pred = torch.zeros(self.n_bptt, 1, bs_sl, self.n_tokens, device=x_enc.device) # (n_bptt, 1, bs_sl, n_tokens)
+        s2_pred = torch.zeros(self.n_bptt, 1, bs_sl, self.n_tokens, device=self.device) # (n_bptt, 1, bs_sl, n_tokens)
 
         # prep s2 state
         cur_x_enc = x_enc # (1, bs_sl, d_model)
@@ -186,22 +186,41 @@ class S2Transformer(L.LightningModule):
         for param in self.vae.parameters():
             param.requires_grad = False
 
-        self.s1_model = S1Model(d_model, n_enc_heads, n_enc_layers, n_tokens, weight_decay, pad_token_id, dropout)
-        self.s2_model = S2Model(self.n_bptt, d_model, n_enc_heads, n_enc_layers, n_tokens, weight_decay, pad_token_id, dropout)
+        self.loop_d_scale = 1
+        loop_h_scale = 1
+        in_h_scale = 1
+        d_vae=128
+
+        self.in_linear = nn.Linear(d_vae, d_model*self.loop_d_scale)
+        self.in_model = nn.TransformerEncoder(mb.TransformerEncoderLayer(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*in_h_scale, activation='gelu', dropout=self.dropout, batch_first=False), num_layers=n_enc_layers*2)
+        self.loop_model = nn.TransformerEncoder(mb.TransformerEncoderLayer(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, activation='gelu', dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+        self.pred_in_linear = nn.Linear(d_model*self.loop_d_scale, d_model)
+        self.pred_model = nn.TransformerEncoder(mb.TransformerEncoderLayer(d_model=d_model, nhead=n_enc_heads, activation='gelu', dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+        self.pred_linear = nn.Linear(d_model, n_tokens)
 
         # metrics
         for mode in ['train', 'val']: # hack: metrics must be on self or Lightning doesn't handle their devices correctly
-            setattr(self, f'{mode}_s1_acc', torchmetrics.classification.Accuracy(task="multiclass", num_classes=n_tokens, ignore_index=pad_token_id))
-            self.metrics[f'{mode}_s1_acc'] = getattr(self, f'{mode}_s1_acc')
-            setattr(self, f'{mode}_s2_lc_acc', torchmetrics.classification.Accuracy(task="multiclass", num_classes=n_tokens, ignore_index=pad_token_id))
-            self.metrics[f'{mode}_s2_lc_acc'] = getattr(self, f'{mode}_s2_lc_acc')
-            setattr(self, f'{mode}_cmb_acc', torchmetrics.classification.Accuracy(task="multiclass", num_classes=n_tokens, ignore_index=pad_token_id))
-            self.metrics[f'{mode}_cmb_acc'] = getattr(self, f'{mode}_cmb_acc')
+            for i in range(n_bptt):
+                setattr(self, f'{mode}_acc_{i}', torchmetrics.classification.Accuracy(task="multiclass", num_classes=n_tokens, ignore_index=pad_token_id))
+                self.metrics[f'{mode}_acc_{i}'] = getattr(self, f'{mode}_acc_{i}')
+
+        self.init_weights()
+
+    def init_weights(self):
+        initrange = 0.1
+        self.in_linear.bias.data.zero_()
+        self.in_linear.weight.data.uniform_(-initrange, initrange)
+
+        self.pred_in_linear.bias.data.zero_()
+        self.pred_in_linear.weight.data.uniform_(-initrange, initrange)
+
+        self.pred_linear.bias.data.zero_()
+        self.pred_linear.weight.data.uniform_(-initrange, initrange)
 
 
     def model_step(self, batch, batch_idx, mode='train'):
-        s1_opt, s2_opt = self.optimizers()
-        s1_lr_sched, s2_lr_sched = self.lr_schedulers()
+        s1_opt = self.optimizers()
+        s1_lr_sched = self.lr_schedulers()
 
         x, y = batch # (bs, seq_len)
         padding_mask = (x==self.pad_token_id) # (bs, seq_len)
@@ -209,96 +228,144 @@ class S2Transformer(L.LightningModule):
 
         # encode to single token with pre-trained, frozen VAE
         x_emb, x_enc = self.vae.forward_enc(x, padding_mask=padding_mask) # (seq_len, bs, d_model)
-        seq_len, bs, d_model = x_enc.shape
+        seq_len, bs, d_vae = x_enc.shape
+        d_model = self.d_model
 
         # since each token is its own row, convert to seq_len=1 for hygiene
         # (this makes padding irrelevant, since cross-entropy loss ignores padding tokens,
         # but it's a good habit to keep the shape consistent)
-        x_enc = x_enc.reshape(1, -1, d_model) # (1, bs*seq_len, d_model)
+        x_enc = x_enc.reshape(1, -1, d_vae) # (1, bs*seq_len, d_vae)
         #y = y.reshape(-1) # (bs*seq_len,)
         padding_mask = padding_mask.reshape(-1,1) # (bs*seq_len,1)
 
-        # ===========================
-        # Predict, optimize S1 model
-        # ===========================
-        s1_pred_enc, s1_pred = self.s1_model(x_enc) # (1, bs*seq_len, d_model), (1, bs*seq_len, n_tokens)
+        # translate VAE inputs into internal format
+        x_wip = self.in_linear(x_enc) # (1, bs*seq_len, 2*d_model)
+        x_wip = self.in_model(x_wip) # (1, bs*seq_len, 2*d_model)
 
-        s1_pred = s1_pred.reshape(seq_len, bs, -1) # (seq_len, bs, n_tokens)
-        s1_pred = s1_pred.permute(1, 0, 2) # (bs, seq_len, n_tokens)
 
-        s1_pred_ce = torch.permute(s1_pred, (0, 2, 1)) # (bs, n_tokens, seq_len)
-        s1_loss = F.cross_entropy(s1_pred_ce, y, ignore_index=self.pad_token_id)
+        # prep tensor to hold loop results
+        x_loops = torch.zeros(self.n_bptt, 1, bs*seq_len, self.d_model*self.loop_d_scale, device=self.device) # (n_bptt, 1, bs*seq_len, 2*d_model)
 
-        # s1_pred_ce = s1_pred.squeeze(0) # (bs*seq_len, n_tokens)
-        # s1_loss = F.cross_entropy(s1_pred_ce, y, ignore_index=self.pad_token_id) # (1,)
+        # run loop model n_loops times
+        s1_loss = torch.tensor(0.0, device=self.device)
+        for i in range(self.n_bptt):
+            x_wip = x_wip + self.loop_model(x_wip) # (1, bs*seq_len, 2*d_model)
+            x_loops[i] = x_wip
+            # x_wip = x_loops[i]
 
-        # log stats
-        _, s1_y_hat = torch.max(s1_pred_ce, dim=1) # (bs*seq_len,)
-        s1_acc = self.metrics[f'{mode}_s1_acc'](s1_y_hat, y)
-        self.log(f"{mode}_s1_acc", s1_acc)
+        # downsize to prediction format
+        x_loops = self.pred_in_linear(x_loops) # (n_bptt, 1, bs*seq_len, d_model)
+
+        # translate internal format into prediction
+        x_loops = x_loops.transpose(0, 1).reshape(1, -1, d_model) # (1, n_bptt*bs*seq_len, d_model)
+        s1_pred_enc = self.pred_model(x_loops) # (1, n_bptt*bs*seq_len, d_model)
+        s1_pred = self.pred_linear(s1_pred_enc) # (1, n_bptt*bs*seq_len, n_tokens)
+
+        # calculate loss
+        s1_pred = s1_pred.reshape(self.n_bptt, seq_len, bs, -1) # (n_bptt, seq_len, bs, n_tokens)
+        s1_pred = s1_pred.permute(0, 2, 1, 3) # (n_bptt, bs, seq_len, n_tokens)
+        s1_pred = s1_pred.reshape(-1, seq_len, self.n_tokens) # (n_bptt*bs, seq_len, n_tokens)
+
+        s1_pred_ce = torch.permute(s1_pred, (0, 2, 1)) # (n_bptt*bs, n_tokens, seq_len)
+        s1_loss = F.cross_entropy(s1_pred_ce, y.repeat(self.n_bptt, 1), ignore_index=self.pad_token_id)
+
+        # log stats for each loop
+        s1_pred = s1_pred.reshape(self.n_bptt*bs, seq_len, -1) # (n_bptt*bs, seq_len, n_tokens)
+        _, s1_y_hat = torch.max(s1_pred, dim=2) # (n_bptt*bs, seq_len)
+        s1_y_hat = s1_y_hat.reshape(self.n_bptt, bs, seq_len) # (n_bptt, bs, seq_len)
+        for i in range(self.n_bptt):
+            s1_acc = self.metrics[f'{mode}_acc_{i}'](s1_y_hat[i], y)
+            self.log(f"{mode}_acc_{i}", s1_acc, sync_dist=True)
+
+        #s1_acc = self.metrics[f'{mode}_acc_{i}'](s1_y_hat, y)
+        #self.log(f"{mode}_acc_{i}", s1_acc)
+
+
+        #     # translate internal format into prediction
+        #     s1_pred_enc = self.pred_model(x_wip) # (1, bs*seq_len, d_model)
+        #     s1_pred = self.pred_linear(s1_pred_enc) # (1, bs*seq_len, n_tokens)
+
+        #     # calculate loss
+        #     s1_pred = s1_pred.reshape(seq_len, bs, -1) # (seq_len, bs, n_tokens)
+        #     s1_pred = s1_pred.permute(1, 0, 2) # (bs, seq_len, n_tokens)
+        #     s1_pred = s1_pred.reshape(-1, seq_len, self.n_tokens) # (bs, seq_len, n_tokens)
+
+        #     s1_pred_ce = torch.permute(s1_pred, (0, 2, 1)) # (bs, n_tokens, seq_len)
+        #     s1_loss += F.cross_entropy(s1_pred_ce, y, ignore_index=self.pad_token_id)
+
+        #     # log stats for each loop
+        #     s1_pred = s1_pred.reshape(bs, seq_len, -1) # (bs, seq_len, n_tokens)
+        #     _, s1_y_hat = torch.max(s1_pred, dim=2) # (bs, seq_len)
+        #     s1_acc = self.metrics[f'{mode}_acc_{i}'](s1_y_hat, y)
+        #     self.log(f"{mode}_acc_{i}", s1_acc)
+
+        # s1_loss /= self.n_bptt
 
         # optimize
         if mode == 'train':
             s1_opt.zero_grad()
             self.manual_backward(s1_loss)
-            torch.nn.utils.clip_grad_norm_(self.s1_model.parameters(), 1.0)
+            torch.nn.utils.clip_grad_norm_(self.parameters(), 5.0)
             s1_opt.step()
             s1_lr_sched.step()
 
-        # ===========================
-        # Pull out low-confidence predictions
-        # ===========================
-
-        s2_fraction = 0.25
-        n_lc = int(bs*seq_len*s2_fraction)
-        s1_prob = F.softmax(s1_pred_enc, dim=2) # (1, bs*seq_len, n_tokens)
-        s1_maxprob, _ = torch.max(s1_prob, dim=2) # (1, bs*seq_len)
-        s1_maxprob_nonpad = s1_maxprob * ~padding_mask.squeeze(1) # (1, bs*seq_len)
-
-        s1_max_idxs = torch.argsort(s1_maxprob_nonpad, descending=True, dim=1)[:,:n_lc] # (n_lc,)
-        lc_x_enc = x_enc[:,s1_max_idxs[0]] # (1, n_lc, d_model)
-        lc_y = y.T.reshape(-1)[s1_max_idxs[0]] # (n_lc,)
+        self.log_dict({"s1_loss": s1_loss}, prog_bar=True, sync_dist=True)
 
 
-        # ===========================
-        # Predict, optimize S2 model
-        # ===========================
+        # # ===========================
+        # # Pull out low-confidence predictions
+        # # ===========================
 
-        s2_lc_pred = self.s2_model(lc_x_enc, self.s1_model) # (n_bptt, 1, n_lc, n_tokens)
+        # s2_fraction = 0.25
+        # n_lc = int(bs*seq_len*s2_fraction)
+        # s1_prob = F.softmax(s1_pred_enc, dim=2) # (1, bs*seq_len, n_tokens)
+        # s1_maxprob, _ = torch.max(s1_prob, dim=2) # (1, bs*seq_len)
+        # s1_maxprob_nonpad = s1_maxprob * ~padding_mask.squeeze(1) # (1, bs*seq_len)
 
-        if not self.bptt_every_loop:
-            s2_lc_pred = s2_lc_pred[-1:] # (1, 1, n_lc, n_tokens)
-            s2_lc_pred_ce = s2_lc_pred.squeeze(0).squeeze(0) # (n_lc, n_tokens)
-        else:
-            s2_lc_pred = s2_lc_pred.squeeze(1)
-            s2_lc_pred_ce = s2_lc_pred.reshape(-1, self.n_tokens) # (n_bptt*n_lc, n_tokens)
-            lc_y = lc_y.repeat(self.n_bptt) # (n_bptt*n_lc,)
+        # s1_max_idxs = torch.argsort(s1_maxprob_nonpad, descending=True, dim=1)[:,:n_lc] # (n_lc,)
+        # lc_x_enc = x_enc[:,s1_max_idxs[0]] # (1, n_lc, d_model)
+        # lc_y = y.T.reshape(-1)[s1_max_idxs[0]] # (n_lc,)
 
-        s2_loss = F.cross_entropy(s2_lc_pred_ce, lc_y, ignore_index=self.pad_token_id)
 
-        # log stats
-        _, s2_lc_y_hat = torch.max(s2_lc_pred_ce, dim=1) # (n_bptt*n_lc,)
-        s2_lc_acc = self.metrics[f'{mode}_s2_lc_acc'](s2_lc_y_hat, lc_y)
-        self.log(f"{mode}_s2_lc_acc", s2_lc_acc)
+        # # ===========================
+        # # Predict, optimize S2 model
+        # # ===========================
 
-        # optimize
-        if mode == 'train':
-            s2_opt.zero_grad()
-            self.manual_backward(s2_loss)
-            torch.nn.utils.clip_grad_norm_(self.s2_model.parameters(), 1.0)
-            s2_opt.step()
-            s2_lr_sched.step()
+        # s2_lc_pred = self.s2_model(lc_x_enc, self.s1_model) # (n_bptt, 1, n_lc, n_tokens)
 
-        # recombine with S1 preds to report overall accuracy
-        cmb_y_hat = s1_y_hat.T.reshape(-1) # (bs*seq_len,)
-        cmb_y_hat[s1_max_idxs[0]] = s2_lc_y_hat[-n_lc:] # (bs*seq_len,): use last bptt period only
-        cmb_acc = self.metrics[f'{mode}_cmb_acc'](cmb_y_hat, y.T.reshape(-1))
-        self.log(f"{mode}_cmb_acc", cmb_acc)
+        # if not self.bptt_every_loop:
+        #     s2_lc_pred = s2_lc_pred[-1:] # (1, 1, n_lc, n_tokens)
+        #     s2_lc_pred_ce = s2_lc_pred.squeeze(0).squeeze(0) # (n_lc, n_tokens)
+        # else:
+        #     s2_lc_pred = s2_lc_pred.squeeze(1)
+        #     s2_lc_pred_ce = s2_lc_pred.reshape(-1, self.n_tokens) # (n_bptt*n_lc, n_tokens)
+        #     lc_y = lc_y.repeat(self.n_bptt) # (n_bptt*n_lc,)
+
+        # s2_loss = F.cross_entropy(s2_lc_pred_ce, lc_y, ignore_index=self.pad_token_id)
+
+        # # log stats
+        # _, s2_lc_y_hat = torch.max(s2_lc_pred_ce, dim=1) # (n_bptt*n_lc,)
+        # s2_lc_acc = self.metrics[f'{mode}_s2_lc_acc'](s2_lc_y_hat, lc_y)
+        # self.log(f"{mode}_s2_lc_acc", s2_lc_acc)
+
+        # # optimize
+        # if mode == 'train':
+        #     s2_opt.zero_grad()
+        #     self.manual_backward(s2_loss)
+        #     torch.nn.utils.clip_grad_norm_(self.s2_model.parameters(), 1.0)
+        #     s2_opt.step()
+        #     s2_lr_sched.step()
+
+        # # recombine with S1 preds to report overall accuracy
+        # cmb_y_hat = s1_y_hat.T.reshape(-1) # (bs*seq_len,)
+        # cmb_y_hat[s1_max_idxs[0]] = s2_lc_y_hat[-n_lc:] # (bs*seq_len,): use last bptt period only
+        # cmb_acc = self.metrics[f'{mode}_cmb_acc'](cmb_y_hat, y.T.reshape(-1))
+        # self.log(f"{mode}_cmb_acc", cmb_acc)
         
-        self.log(f"{mode}_loss_delta", s1_loss - s2_loss)
-        self.log(f"{mode}_acc_delta", cmb_acc - s1_acc)
+        # self.log(f"{mode}_loss_delta", s1_loss - s2_loss)
+        # self.log(f"{mode}_acc_delta", cmb_acc - s1_acc)
 
-        self.log_dict({"s1_loss": s1_loss, "s2_loss": s2_loss}, prog_bar=True)
+        # self.log_dict({"s1_loss": s1_loss, "s2_loss": s2_loss}, prog_bar=True)
 
     def training_step(self, batch, batch_idx):
         return self.model_step(batch, batch_idx, mode='train')
@@ -319,12 +386,12 @@ class S2Transformer(L.LightningModule):
         self.log_and_reset_metrics('val')
 
     def configure_optimizers(self):
-        s1_opt = torch.optim.AdamW(self.s1_model.parameters(), lr=self.lr_s1, weight_decay=self.wd)
-        s2_opt = torch.optim.AdamW(self.s2_model.parameters(), lr=self.lr_s2, weight_decay=self.wd)
+        s1_opt = torch.optim.AdamW(self.parameters(), lr=self.lr_s1, weight_decay=self.wd)
+        # s2_opt = torch.optim.AdamW(self.s2_model.parameters(), lr=self.lr_s2, weight_decay=self.wd)
         s1_lr_sched = pl_bolts.optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(
             s1_opt, warmup_epochs=2000, max_epochs=100000, warmup_start_lr=0.0, eta_min=0.1*self.lr_s1, last_epoch=-1)
-        s2_lr_sched = pl_bolts.optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(
-            s2_opt, warmup_epochs=2000, max_epochs=100000, warmup_start_lr=0.0, eta_min=0.1*self.lr_s2, last_epoch=-1)
+        # s2_lr_sched = pl_bolts.optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(
+        #     s2_opt, warmup_epochs=2000, max_epochs=100000, warmup_start_lr=0.0, eta_min=0.1*self.lr_s2, last_epoch=-1)
 
-        return [s1_opt, s2_opt], [s1_lr_sched, s2_lr_sched] 
+        return [s1_opt], [s1_lr_sched] # [s1_opt, s2_opt], [s1_lr_sched, s2_lr_sched] 
 
