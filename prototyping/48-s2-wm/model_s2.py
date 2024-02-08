b916a533428d68e72ad29ea5a7e994779910b1ab
Derek Chong <derekch@jagupard38.stanford.edu>
2024-02-08 12:37:54 -0800

    POC of new arch in 48

--- prototyping/48-s2-wm/model_s2.py
+++ prototyping/48-s2-wm/model_s2.py
@@ -48,30 +48,35 @@ class S2Transformer(L.LightningModule):
         self.in_enc = nn.TransformerEncoder(mb.TransformerEncoderLayer(d_model=d_model, nhead=n_enc_heads, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
         # self.in_linear = nn.Linear(d_vae, d_model*self.loop_d_scale)
 
-        self.loop_starter = nn.TransformerEncoder(mb.TransformerEncoderLayer(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+        self.s1 = nn.TransformerEncoder(mb.TransformerEncoderLayer(d_model=d_model, nhead=n_enc_heads, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+        self.s2 = nn.TransformerDecoder(mb.TransformerDecoderLayer(d_model=d_model, nhead=n_enc_heads, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers*2)
 
-        # self.loop_model_1 = nn.TransformerDecoder(_TransformerDecoderLayerSwiGLU(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
-        # self.loop_model_2 = nn.TransformerDecoder(_TransformerDecoderLayerSwiGLU(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
-        # self.loop_model_3 = nn.TransformerDecoder(_TransformerDecoderLayerSwiGLU(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
-        # self.loop_model_4 = nn.TransformerDecoder(_TransformerDecoderLayerSwiGLU(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
-        # self.loop_model = model_xf.get_decoder(d_model=d_model*self.loop_d_scale)
-        #self.loop_model = nn.TransformerDecoder(_TransformerDecoderLayerSwiGLU(d_model=d_model*self.loop_d_scale, dim_feedforward=d_loop_feed_forward, nhead=n_enc_heads*loop_h_scale, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
-        self.loop_model = nn.TransformerDecoder(mb.TransformerDecoderLayer(d_model=d_model*self.loop_d_scale, dim_feedforward=d_loop_feed_forward, nhead=n_enc_heads*loop_h_scale, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_loop_layers)
-        # self.loop_wm    = nn.TransformerEncoder(mb.TransformerEncoderLayer(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
 
-        self.pred_in_linear = nn.Linear(d_model*self.loop_d_scale, d_model)
+        # # self.loop_starter = nn.TransformerEncoder(mb.TransformerEncoderLayer(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+
+        # # self.loop_model_1 = nn.TransformerDecoder(_TransformerDecoderLayerSwiGLU(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+        # # self.loop_model_2 = nn.TransformerDecoder(_TransformerDecoderLayerSwiGLU(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+        # # self.loop_model_3 = nn.TransformerDecoder(_TransformerDecoderLayerSwiGLU(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+        # # self.loop_model_4 = nn.TransformerDecoder(_TransformerDecoderLayerSwiGLU(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+        # # self.loop_model = model_xf.get_decoder(d_model=d_model*self.loop_d_scale)
+        # #self.loop_model = nn.TransformerDecoder(_TransformerDecoderLayerSwiGLU(d_model=d_model*self.loop_d_scale, dim_feedforward=d_loop_feed_forward, nhead=n_enc_heads*loop_h_scale, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+        # self.loop_model = nn.TransformerDecoder(mb.TransformerDecoderLayer(d_model=d_model*self.loop_d_scale, dim_feedforward=d_loop_feed_forward, nhead=n_enc_heads*loop_h_scale, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_loop_layers)
+        # # self.loop_wm    = nn.TransformerEncoder(mb.TransformerEncoderLayer(d_model=d_model*self.loop_d_scale, nhead=n_enc_heads*loop_h_scale, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+
+        # self.pred_in_linear = nn.Linear(d_model*self.loop_d_scale, d_model)
 
 
         self.backprop_to_ingest = False # Leave as False. After testing, True starts better but eventually perf is consistently worse (0.3% absolute at the 0.77 mark)
 
-
         self.pred_linear = nn.Linear(d_model, n_tokens)
-        if self.n_preds > 1:
-            self.pred_tf = nn.TransformerDecoder(mb.TransformerDecoderLayer(d_model=d_model, nhead=n_enc_heads, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
-            dummy_s2_starters = (torch.rand_like(torch.zeros(self.n_preds,1,d_model)) * (self.initrange*2)) - self.initrange
-            self.dummy_s2_starters = torch.nn.Parameter(dummy_s2_starters) # (n_preds,1,d_model)
-        else:
-            self.pred_model = nn.TransformerEncoder(mb.TransformerEncoderLayer(d_model=d_model, nhead=n_enc_heads, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+        self.pred_linear_s2proj = nn.Linear(n_tokens, d_model)
+        # if self.n_preds > 1:
+        #     self.pred_tf = nn.TransformerDecoder(mb.TransformerDecoderLayer(d_model=d_model, nhead=n_enc_heads, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+        #     dummy_s2_starters = (torch.rand_like(torch.zeros(self.n_preds,1,d_model)) * (self.initrange*2)) - self.initrange
+        #     self.dummy_s2_starters = torch.nn.Parameter(dummy_s2_starters) # (n_preds,1,d_model)
+        # else:
+        #     self.pred_model = nn.TransformerEncoder(mb.TransformerEncoderLayer(d_model=d_model, nhead=n_enc_heads, activation=F.leaky_relu, dropout=self.dropout, batch_first=False), num_layers=n_enc_layers)
+
 
         dummy_wm = (torch.rand_like(torch.zeros(1,1,d_model)) * (self.initrange*2)) - self.initrange # (1,1,d_model)
         self.dummy_wm = torch.nn.Parameter(dummy_wm) # (1,1,d_model)
@@ -82,22 +87,41 @@ class S2Transformer(L.LightningModule):
                 setattr(self, f'{mode}_acc_{i:02}', torchmetrics.classification.Accuracy(task="multiclass", num_classes=n_tokens, ignore_index=pad_token_id))
                 self.metrics[f'{mode}_acc_{i:02}'] = getattr(self, f'{mode}_acc_{i:02}')
 
+        # setup hooks to get activations from each layer of s1_model
+        self.hooks = []
+        self.activations = [None] * n_enc_layers
+        def get_hook(i):
+            def hook(module, input, output):
+                self.activations[i] = output.reshape(1, -1, d_model) # (1, bs*seq_len, d_model)
+            return hook
+        for i in range(len(self.s1.layers)):
+            self.hooks.append(self.s1.layers[i].register_forward_hook(get_hook(i)))
+
+
+
         self.init_weights()
+
+        self.s1_params = list(self.s1.parameters()) + list(self.in_enc.parameters()) + list(self.embedding.parameters()) + list(self.pred_linear.parameters())
+        self.s2_params = list(self.s2.parameters()) + [self.dummy_wm] + list(self.pred_linear_s2proj.parameters())
+
 
     def init_weights(self):
         initrange = self.initrange
         #self.in_linear.bias.data.zero_()
         #self.in_linear.weight.data.uniform_(-initrange, initrange)
 
-        self.pred_in_linear.bias.data.zero_()
-        self.pred_in_linear.weight.data.uniform_(-initrange, initrange)
+        # self.pred_in_linear.bias.data.zero_()
+        # self.pred_in_linear.weight.data.uniform_(-initrange, initrange)
 
         self.pred_linear.bias.data.zero_()
         self.pred_linear.weight.data.uniform_(-initrange, initrange)
 
+        self.pred_linear_s2proj.bias.data.zero_()
+        self.pred_linear_s2proj.weight.data.uniform_(-initrange, initrange)
+
     def model_step(self, batch, batch_idx, mode='train'):
-        s1_opt = self.optimizers()
-        s1_lr_sched = self.lr_schedulers()
+        s1_opt, s2_opt = self.optimizers()
+        s1_lr_sched, s2_lr_sched = self.lr_schedulers()
 
         x, y = batch # (bs, seq_len)
         padding_mask = (x==self.pad_token_id) # (bs, seq_len)
@@ -124,39 +148,104 @@ class S2Transformer(L.LightningModule):
         #x_in = self.in_linear(x_enc) # (1, bs*seq_len, 2*d_model)
         #x_in = self.in_model(x_in) # (1, bs*seq_len, 2*d_model)
 
-        # prime the pump: take a first glance at the input and produce the internal representation
-        # for loops to come
-        dummy_wm = self.dummy_wm.repeat(1,bs*seq_len,1) # (1, bs*seq_len, 2*d_model)
-        x_wip = self.loop_starter(x_in) # (1, bs*seq_len, 2*d_model)
-        x_wip = torch.cat((x_wip, dummy_wm), dim=0) # (2, bs*seq_len, 2*d_model)
+        # freeze s1_model
+        for param in self.s1.parameters():
+            param.requires_grad = True
+        # make s1 predictions
+        s1_pred_enc = self.s1(x_in) # (1, bs*seq_len, d_model)
+        s1_pred = self.pred_linear(s1_pred_enc) # (1, bs*seq_len, n_tokens)
+        # s1 loss
+        s1_pred_ce = s1_pred.reshape(seq_len, bs, -1) # (seq_len, bs, n_tokens)
+        s1_pred_ce = s1_pred_ce.permute(1, 2, 0) # (bs, n_tokens, seq_len)
+        s1_loss = F.cross_entropy(s1_pred_ce, y, ignore_index=self.pad_token_id, reduction='mean') # (1,)
+        # log stats
+        _, s1_y_hat = torch.max(s1_pred_ce, dim=1) # (bs, seq_len)
+        s1_acc = self.metrics[f'{mode}_acc_00'](s1_y_hat, y)
+        # optimize
+        if mode == 'train':
+            s1_opt.zero_grad()
+            self.manual_backward(s1_loss)
+            torch.nn.utils.clip_grad_norm_(self.s1_params, 5.0)
+            if self.step % self.grad_acc == 0:
+                s1_opt.step()
+                s1_lr_sched.step()
 
-        # x_in = x_enc
-        # x_wip = x_in
+        # freeze s1_model
+        for param in self.s1.parameters():
+            param.requires_grad = False
+        # grab s1 activations
+        s1_acts = torch.cat(self.activations, dim=0).detach() # (n_enc_layers, bs*seq_len, d_model)
+        s1_linear_preds = self.pred_linear_s2proj(s1_pred).detach() # (1, bs*seq_len, d_model)
 
-        # prep tensor to hold loop results
-        x_loops = torch.zeros(self.n_bptt, 1, bs*seq_len, self.d_model*self.loop_d_scale, device=self.device) # (n_bptt, 1, bs*seq_len, 2*d_model)
+        s1_data = torch.cat((x_in, s1_acts, s1_linear_preds), dim=0).detach() # (n_enc_layers+2, bs*seq_len, d_model
+        # s2: predict a better S1 input
+        s2_starter = self.dummy_wm.repeat(1,bs*seq_len,1) # (1, bs*seq_len, d_model)
+        s2_new_s1 = self.s2(s2_starter, s1_data) # (1, bs*seq_len, d_model)
+        # push through S1 again
+        s2_pred_enc = self.s1(s2_new_s1) # (1, bs*seq_len, d_model)
+        s2_pred = self.pred_linear(s2_pred_enc) # (1, bs*seq_len, n_tokens)
+        # s2
+        s2_pred = s2_pred.reshape(seq_len, bs, -1) # (seq_len, bs, n_tokens)
+        s2_pred = s2_pred.permute(1, 2, 0) # (bs, n_tokens, seq_len)
+        s2_loss = F.cross_entropy(s2_pred, y, ignore_index=self.pad_token_id, reduction='mean') # (1,)
 
-        # # run loop model n_loops times
-        # x_wip = x_wip + self.loop_model_1(x_wip, x_in) # (1, bs*seq_len, 2*d_model)
-        # x_loops[0] = x_wip
-        # x_wip = x_wip + self.loop_model_2(x_wip, x_in) # (1, bs*seq_len, 2*d_model)
-        # x_loops[1] = x_wip
-        # x_wip = x_wip + self.loop_model_3(x_wip, x_in) # (1, bs*seq_len, 2*d_model)
-        # x_loops[2] = x_wip
-        # x_wip = x_wip + self.loop_model_4(x_wip, x_in) # (1, bs*seq_len, 2*d_model)
-        # x_loops[3] = x_wip
+        # log stats
+        _, s2_y_hat = torch.max(s2_pred, dim=1) # (bs, seq_len)
+        s2_acc = self.metrics[f'{mode}_acc_00'](s2_y_hat, y)
 
+        # optimize
+        if mode == 'train':
+            s2_opt.zero_grad()
+            self.manual_backward(s2_loss)
+            torch.nn.utils.clip_grad_norm_(self.s2_params, 5.0)
+            if self.step % self.grad_acc == 0:
+                s2_opt.step()
+                s2_lr_sched.step()
 
-        for i in range(self.n_bptt):
-            x_wip_next = self.loop_model(x_wip, x_in) # (i+1, bs*seq_len, 2*d_model)
-            next_wm = x_wip_next[-1:] # (1, bs*seq_len, 2*d_model)
-            x_loops[i] = next_wm
-            x_wip = torch.cat((next_wm, x_wip), dim=0) # (i+2, bs*seq_len, 2*d_model)
+            self.step = (self.step + 1) % self.grad_acc
+
+        self.log_dict({"s1_loss": s1_loss, "s2_loss": s2_loss, "s1_acc": s1_acc, "s2_acc": s2_acc}, prog_bar=True, sync_dist=True)
+
+        return
+
         
-        bpti = 0
-        if self.backprop_to_ingest:
-            x_loops = torch.cat((x_in.unsqueeze(0), x_loops), dim=0) # (n_bptt+1, 1, bs*seq_len, 2*d_model)
-            bpti = 1
+
+
+
+
+        # # prime the pump: take a first glance at the input and produce the internal representation
+        # # for loops to come
+        # dummy_wm = self.dummy_wm.repeat(1,bs*seq_len,1) # (1, bs*seq_len, 2*d_model)
+        # x_wip = self.loop_starter(x_in) # (1, bs*seq_len, 2*d_model)
+        # x_wip = torch.cat((x_wip, dummy_wm), dim=0) # (2, bs*seq_len, 2*d_model)
+
+        # # x_in = x_enc
+        # # x_wip = x_in
+
+        # # prep tensor to hold loop results
+        # x_loops = torch.zeros(self.n_bptt, 1, bs*seq_len, self.d_model*self.loop_d_scale, device=self.device) # (n_bptt, 1, bs*seq_len, 2*d_model)
+
+        # # # run loop model n_loops times
+        # # x_wip = x_wip + self.loop_model_1(x_wip, x_in) # (1, bs*seq_len, 2*d_model)
+        # # x_loops[0] = x_wip
+        # # x_wip = x_wip + self.loop_model_2(x_wip, x_in) # (1, bs*seq_len, 2*d_model)
+        # # x_loops[1] = x_wip
+        # # x_wip = x_wip + self.loop_model_3(x_wip, x_in) # (1, bs*seq_len, 2*d_model)
+        # # x_loops[2] = x_wip
+        # # x_wip = x_wip + self.loop_model_4(x_wip, x_in) # (1, bs*seq_len, 2*d_model)
+        # # x_loops[3] = x_wip
+
+
+        # for i in range(self.n_bptt):
+        #     x_wip_next = self.loop_model(x_wip, x_in) # (i+1, bs*seq_len, 2*d_model)
+        #     next_wm = x_wip_next[-1:] # (1, bs*seq_len, 2*d_model)
+        #     x_loops[i] = next_wm
+        #     x_wip = torch.cat((x_wip[:-1], next_wm, x_wip[-1:]), dim=0) # (i+2, bs*seq_len, 2*d_model)
+        
+        # bpti = 0
+        # if self.backprop_to_ingest:
+        #     x_loops = torch.cat((x_in.unsqueeze(0), x_loops), dim=0) # (n_bptt+1, 1, bs*seq_len, 2*d_model)
+        #     bpti = 1
 
         # downsize to prediction format
         x_loops = self.pred_in_linear(x_loops) # (n_bptt+bpti, 1, bs*seq_len, d_model)
@@ -329,12 +418,12 @@ class S2Transformer(L.LightningModule):
         self.log_and_reset_metrics('val')
 
     def configure_optimizers(self):
-        s1_opt = torch.optim.AdamW(self.parameters(), lr=self.lr_s1, weight_decay=self.wd)
-        # s2_opt = torch.optim.AdamW(self.s2_model.parameters(), lr=self.lr_s2, weight_decay=self.wd)
+        s1_opt = torch.optim.AdamW(self.s1_params, lr=self.lr_s1, weight_decay=self.wd)
+        s2_opt = torch.optim.AdamW(self.s2_params, lr=self.lr_s2, weight_decay=self.wd)
         s1_lr_sched = pl_bolts.optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(
             s1_opt, warmup_epochs=2000, max_epochs=100000, warmup_start_lr=0.0, eta_min=0.1*self.lr_s1, last_epoch=-1)
-        # s2_lr_sched = pl_bolts.optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(
-        #     s2_opt, warmup_epochs=2000, max_epochs=100000, warmup_start_lr=0.0, eta_min=0.1*self.lr_s2, last_epoch=-1)
+        s2_lr_sched = pl_bolts.optimizers.lr_scheduler.LinearWarmupCosineAnnealingLR(
+            s2_opt, warmup_epochs=2000, max_epochs=100000, warmup_start_lr=0.0, eta_min=0.1*self.lr_s2, last_epoch=-1)
 
-        return [s1_opt], [s1_lr_sched] # [s1_opt, s2_opt], [s1_lr_sched, s2_lr_sched] 
+        return [s1_opt, s2_opt], [s1_lr_sched, s2_lr_sched] # [s1_opt, s2_opt], [s1_lr_sched, s2_lr_sched] 
 
