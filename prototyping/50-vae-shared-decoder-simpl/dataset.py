2ab8bfe423aafb33cfaab8d5d5348d779ea18d9a
Derek Chong <dc@derekchong.com>
2024-03-02 04:04:57 +0000

    Groundwork for unsupervised S2

---
+++ prototyping/50-vae-shared-decoder-simpl/dataset.py
@@ -0,0 +1,126 @@
+import torch
+import os, random
+from datasets import load_dataset
+import tokenizers
+
+dataset_path = 'data_utils/ds_addmult_mod10_data/data-addmult-231102-1.5m.csv'
+# dataset_path = '/dev/shm/lichess_100mb.csv'
+
+
+# define data format
+supported_chars = '0123456789+*()[]abcxyzLFI<=_'
+itos = {i:c for i, c in enumerate(supported_chars)}
+stoi = {c:i for i, c in enumerate(supported_chars)}
+pad_token_id = stoi['_']
+
+# # add chess positions as separate tokens
+# for letter in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']:
+#     for num in range(1, 9):
+#         position = letter + str(num)
+#         itos[len(itos)] = position
+#         stoi[position] = len(stoi)
+
+# from tokenizers import normalizers
+# from tokenizers.normalizers import NFD, StripAccents
+# normalizer = normalizers.Sequence([NFD(), StripAccents()])
+
+
+# setup tokenizer
+tokenizer_model = tokenizers.models.WordLevel(stoi)
+tokenizer = tokenizers.Tokenizer(tokenizer_model)
+
+# tokenizer.pre_tokenizer = tokenizers.pre_tokenizers.Sequence([
+#     tokenizers.pre_tokenizers.Split(tokenizers.Regex("[a-h][1-8]"), behavior="isolated", invert=True),
+#     tokenizers.pre_tokenizers.Whitespace(),
+#     tokenizers.pre_tokenizers.Split('x', behavior="isolated"),
+#     tokenizers.pre_tokenizers.Split(tokenizers.Regex("[A-Z]"), behavior="isolated"),
+#     tokenizers.pre_tokenizers.Split(tokenizers.Regex("^[0-9]"), behavior="isolated"),
+# ])
+# test_tok = tokenizer.pre_tokenizer.pre_tokenize_str("1.e4 g6 2.d4 Bg7 3.Nc3 c5 4.Be3 cxd4 5.Bxd4 Nf6 6.f3 Nc6 7.Be3 O-O 8.Qd2 a6 9.O-O-O b5 10.Bh6 b4 11.Nd5 Bb7 12.Bxg7 Kxg7 13.h4 h5 14.g4 a5 15.Nxf6 Kxf6 16.gxh5 Rh8 17.Qg5+ Kg7 18.hxg6 f6 19.Qg3 Ne5 20.h5 Rh6 21.Nh3 Qc7 22.Nf4 Rc8 23.Rh2 a4 24.Kb1 a3 25.Qe1 axb2 26.Kxb2 Nc4+ 27.Bxc4 Qxc4 28.Rxd7 Ra8 29.Rxe7+ Kf8 30.Rf7+ Kg8 31.Qa1 Qc3+ 32.Kb1 Qe1+ 33.Kb2 Qc3+ 34.Kb1 Qe3 35.Ne6 Qg1+ 36.Kb2 Qxh2 37.Rxb7 Qe5+ 38.Kb1 Qxe6 39.Qb2 Qe5")
+# print(test_tok)
+# for tok in test_tok:
+#     if len(tok[0]) > 1: 
+#         print(tok)
+
+tokenizer.pre_tokenizer = tokenizers.pre_tokenizers.Split('', behavior="isolated", invert=True)
+# tokenizer.enable_padding(pad_id=pad_token_id) # do not pad when tokenizing at the load step
+
+def tokenize_csv_rows(rows):
+    tokenized = tokenizer.encode_batch(rows['example'])
+    tok_ids = [t.ids for t in tokenized]
+    lengths = [len(t.ids) for t in tokenized]
+    answers = rows['ans_mod10']
+    ans_sublabels = tokenizer.encode_batch(rows['ans_sublabels'])
+    ans_sublabels = [t.ids for t in ans_sublabels]
+    return {'input_ids': tok_ids, 'lengths': lengths, 'answers': answers, 'ans_sublabels': ans_sublabels}
+
+def detokenize(id_tensor):
+    return tokenizer.decode_batch(id_tensor.tolist())
+
+# height,width,example,answer,ans_mod10,tree_sig,ans_sublabels
+# 2,5,(*14),4,4,(odd),____4
+# 5,21,(+(+33)(+5(*0(+47)))),11,1,(o(odd)(od(od(odd)))),______6__________1051
+
+
+
+# load raw CSV
+def make_datasets(n_train, n_val, num_proc=8, random_seed=2357):
+    """
+    n_train: number of training examples
+    n_val: number of validation examples
+    """
+
+    dataset = load_dataset("csv", data_files=dataset_path, split=f"train[:{n_train+n_val}]")
+
+    dataset = dataset.map(tokenize_csv_rows, batched=True, num_proc=num_proc) # , load_from_cache_file=False)
+    dataset = dataset.select_columns(["input_ids", "answers", "ans_sublabels"])
+    dataset.set_format(type="torch", columns=["input_ids", "answers", "ans_sublabels"])
+
+    dataset = dataset.train_test_split(
+        train_size=n_train, test_size=n_val, 
+        seed=random_seed, shuffle=True
+    )
+
+    ds_train = dataset["train"]
+    ds_val   = dataset["test"]
+
+    return ds_train, ds_val
+
+
+
+def collate_fn(batch):
+
+    min_seq = 64 # hack so model.py's "trim n_rand_encs to be a multiple of bs" doesn't crash
+    max_seq = 512
+
+    lengths = [len(x['input_ids']) for x in batch]
+    longest_seq = min(max(max(lengths), min_seq), max_seq) # longest supported sequence
+    n_items = len(batch)
+
+    batch_x = torch.zeros((n_items, longest_seq), dtype=torch.int8) + pad_token_id
+    batch_sl = torch.zeros((n_items, longest_seq), dtype=torch.int8) + pad_token_id
+    batch_y = torch.stack([x['answers'] for x in batch])
+
+    for i, b in enumerate(batch):
+        batch_x[i, :lengths[i]] = b['input_ids']
+        batch_sl[i,:lengths[i]] = b['ans_sublabels']
+
+    return batch_x, batch_y, batch_sl
+
+
+
+class SeqLenAwareBatchSampler(torch.utils.data.Sampler):
+    def __init__(self, data, batch_size, shuffle):
+        self.data = data
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+    def __len__(self):
+        return (len(self.data) + self.batch_size - 1) // self.batch_size
+    def __iter__(self):
+        sizes = self.data['lengths']
+        index_chunks = list(torch.chunk(torch.argsort(sizes), len(self)))
+        if self.shuffle:
+            random.shuffle(index_chunks)
+        for batch in index_chunks:
+            yield batch.tolist()
+
---
+++ prototyping/50-vae-shared-decoder-simpl/main.py
@@ -0,0 +1,96 @@
+# 41-vae-shared-decoder
+# This is just like 40-vae, except we prep S1 for next-word prediction:
+# we can now parallelize S1 using an encoder. We set up chess.
+
+import torch, torch.nn as nn, torch.utils.data as data, torch.nn.functional as F
+import lightning as L
+import os, time
+import pandas as pd
+from pytorch_lightning.loggers import WandbLogger
+
+import model, model_s2, dataset
+
+# wandb setup
+wandb_logger = WandbLogger(log_model=False)
+
+# GPU setup
+torch.set_float32_matmul_precision('medium')
+
+
+# hyperparameters
+hparams = {
+    'bs': 64,
+    'pad_token_id': dataset.pad_token_id,
+    'cpu_procs': 8,
+
+    # dataset
+    'val_check_interval': 1500, # in steps
+    'n_train': 1450000,     
+    'n_val': 512,     
+
+    # model
+    's1_checkpoint': None, # "../checkpoints/model-epoch=01-step=00567500.ckpt", # None for fresh train
+    's2_mode': False,
+}
+# reminder: unlike main framework, here we are plugging test into val
+# because Lightning (correctly) doesn't have a test step during training
+
+
+hparams['model_hparams'] = {
+    'd_model': 384,
+    'n_enc_heads': 12,
+    'n_enc_layers': 12, 
+    'n_tokens': len(dataset.stoi), # 32 for chess
+    'lr': 5e-5,
+    'weight_decay': 0.1,
+    'pad_token_id': hparams['pad_token_id'],
+    'predictive': True,
+    'dropout': 0.0,
+    'n_bptt': 4,
+}
+
+
+# log all hyperparameters
+wandb_logger.log_hyperparams(hparams)
+
+
+# setup data
+ds_train, ds_val = dataset.make_datasets(hparams['n_train'], hparams['n_val'], num_proc=hparams['cpu_procs'])
+collate_fn = dataset.collate_fn
+#sampler_train = dataset.SeqLenAwareBatchSampler(ds_train, hparams['bs'], shuffle=True)
+#sampler_val = dataset.SeqLenAwareBatchSampler(ds_val, hparams['bs'], shuffle=True)
+dl_train = data.DataLoader(ds_train, collate_fn=collate_fn, pin_memory=True, num_workers=hparams['cpu_procs'], batch_size=hparams['bs'], shuffle=True)
+dl_val = data.DataLoader(ds_val, collate_fn=collate_fn, pin_memory=True, num_workers=hparams['cpu_procs'], batch_size=hparams['bs'], shuffle=False)
+
+# model
+
+if hparams['s1_checkpoint'] is None:
+    s1_model = model.S1Transformer(**hparams['model_hparams'])
+else:
+    s1_model = model.S1Transformer.load_from_checkpoint(hparams['s1_checkpoint'])
+
+if hparams['s2_mode']:
+    main_model = model_s2.S2Transformer(s1_model, **hparams['model_hparams'])
+else:
+    main_model = s1_model
+
+
+# checkpointing
+time_secs = int(time.time())
+fname_prefix = f"model-s1-{time_secs}"
+checkpoint_callback = L.pytorch.callbacks.ModelCheckpoint(
+    monitor='val_pred_loss',
+    every_n_train_steps=2500,
+    dirpath='../checkpoints',
+    filename=fname_prefix+'-{epoch:02d}-{step:08d}',
+    save_top_k=3,
+    mode='min',
+)
+
+# training
+trainer = L.Trainer(accelerator='gpu', logger=wandb_logger, val_check_interval=hparams['val_check_interval'], 
+                    gradient_clip_val=1.0, callbacks=[checkpoint_callback], devices=[0])
+trainer.fit(main_model, dl_train, dl_val)
+
+
+
